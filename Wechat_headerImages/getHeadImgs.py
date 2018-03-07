84c4910ec73583e30e1569a9675eb208f9a34380
lyon <flowingsun007@163.com>
2018-03-07 09:54:40 +0800

    Wechat_headerImages

---
+++ Wechat_headerImages/getHeadImgs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#_*_ coding:utf-8 _*_
+#__author__='阳光流淌007'
+#__date__ = '2018-03-06'
+import itchat
+itchat.auto_login(True)
+for friend in itchat.get_friends(update=True)[0:]:
+    #print(friend['NickName'],friend['RemarkName'],friend['Sex'],friend['Province'],friend['Signature'])
+    img = itchat.get_head_img(userName=friend["UserName"])
+    path = "/Users/zhaoluyang/PythonProject/WeChat_Itchat/HeadImages/"+friend['NickName']+"("+friend['RemarkName']+").jpg"
+    with open(path,'wb') as f:
+        f.write(img)
