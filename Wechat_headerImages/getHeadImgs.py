50d8d41be8f508ed253d4f8b13bbc65d8a5c1eaf
lyon <flowingsun007@163.com>
2018-03-07 17:42:55 +0800

    download all your Wechatfriends' head images

--- Wechat_headerImages/getHeadImgs.py
+++ Wechat_headerImages/getHeadImgs.py
@@ -3,10 +3,11 @@
 #__author__='阳光流淌007'
 #__date__ = '2018-03-06'
 import itchat
-itchat.auto_login(True)
+itchat.auto_login()
 for friend in itchat.get_friends(update=True)[0:]:
     print(friend['NickName'],friend['RemarkName'],friend['Sex'],friend['Province'],friend['Signature'])
     img = itchat.get_head_img(userName=friend["UserName"])
     path = "/Users/xxx/Wechat_headerImages/HeadImages/"+friend['NickName']+"("+friend['RemarkName']+").jpg"
     with open(path,'wb') as f:
         f.write(img)
+itchat.run()
